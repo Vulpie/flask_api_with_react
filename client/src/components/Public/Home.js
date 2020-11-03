e18293b9dc7672855cd576ee4cb9c34ecbd61ce0
Vulpie <wojtas_f@outlook.com>
2020-11-03 22:25:50 +0100

    feat: display posts on home page

--- client/src/components/Public/Home.js
+++ client/src/components/Public/Home.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+
 const Home = () => {
+	const [blog, setBlog] = useState([])
+
+	useEffect(() => {
+		fetch('/api/post/all')
+			.then((res) => res.json())
+			.then((data) => setBlog(data))
+	}, [])
 	return (
 		<>
 			<NavBar />
 			<div className="home">
 				<h2>Home Page</h2>
+				<div className="home__blog">
+					{blog.map((post) => (
+						<div key={'post' + post.id}>
+							{post.title} : {post.content} : {post.createdAt} :{' '}
+							{post.modifiedAt}
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	)
